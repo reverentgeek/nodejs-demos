0433f85ec2c93f943bec643f0450e34877a48287
David Neal <david@reverentgeek.com>
2015-05-14 07:56:43 -0400

    Updating Express Demos for MVP V-Conf

---
+++ ExpressMvp/README.md
@@ -0,0 +1,3 @@
+﻿# ExpressMvp
+
+
---
+++ ExpressMvp/routes/index.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'MVP V-Conf' });
+});
+
+module.exports = router;
