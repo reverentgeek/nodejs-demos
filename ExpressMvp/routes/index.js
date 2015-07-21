a04093303c8565e9301d310e5b3a59b2c1fbe976
David Neal <david@reverentgeek.com>
2015-07-21 10:44:24 -0400

    Updating demo

--- ExpressMvp/routes/index.js
+++ ExpressMvp/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    
-    res.render('index', { title: 'CodeStock' });
+
+    res.render('index', { title: 'Attendees' });
 
 });
 
